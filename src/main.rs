ebcd083507d3066d7f90ed3e2ed304e32d986710
Darlan Jr <kelgaxd@gmail.com>
2023-11-20 01:48:55 -0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use opencv::{
+    Result,
+    prelude::*,
+    videoio,
+    highgui
+};
+
+fn main() -> Result<()> {
+    
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
+    highgui::named_window("window", highgui::WINDOW_FULLSCREEN)?;
+    
+    let mut frame = Mat::default();
+
+    loop {
+        cam.read(&mut frame)?;
+        highgui::imshow("window", &frame)?;
+        let key = highgui::wait_key(1)?;
+
+        if key == 113 {
+            break;
+        }
+    }
+
+    Ok(())
 }
